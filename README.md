0aed1c8426cd7bd2715aa731f805d40a9db66e50
Zoltan Kochan <z@kochan.io>
2017-11-28 22:36:13 +0200

    feat: create

---
+++ README.md
@@ -0,0 +1,32 @@
+# @pnpm/tarball-resolver
+
+> Resolver for tarball dependencies
+
+<!--@shields('npm', 'travis')-->
+[![npm version](https://img.shields.io/npm/v/@pnpm/tarball-resolver.svg)](https://www.npmjs.com/package/@pnpm/tarball-resolver) [![Build Status](https://img.shields.io/travis/pnpm/tarball-resolver/master.svg)](https://travis-ci.org/pnpm/tarball-resolver)
+<!--/@-->
+
+## Installation
+
+```sh
+npm i -S @pnpm/tarball-resolver
+```
+
+## Usage
+
+<!--@example('./example.js')-->
+```js
+'use strict'
+const resolveFromTarball = require('@pnpm/tarball-resolver').default
+
+resolveFromTarball({pref: 'http://registry.npmjs.org/is-array/-/is-array-1.0.1.tgz'})
+  .then(resolveResult => console.log(resolveResult))
+  //> { id: 'registry.npmjs.org/is-array/1.0.1',
+  //    normalizedPref: 'http://registry.npmjs.org/is-array/-/is-array-1.0.1.tgz',
+  //    resolution: { tarball: 'http://registry.npmjs.org/is-array/-/is-array-1.0.1.tgz' } }
+```
+<!--/@-->
+
+## License
+
+[MIT](./LICENSE) © [Zoltan Kochan](https://www.kochan.io/)
---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import parseNpmTarballUrl from 'parse-npm-tarball-url'
+
+export default async function resolveTarball (
+  wantedDependency: {pref: string},
+) {
+  if (!wantedDependency.pref.startsWith('http:') && !wantedDependency.pref.startsWith('https:')) {
+    return null
+  }
+
+  if (wantedDependency.pref.startsWith('http://registry.npmjs.org/')) {
+    const parsed = parseNpmTarballUrl(wantedDependency.pref)
+    if (parsed) {
+      return {
+        id: `${parsed.host}/${parsed.pkg.name}/${parsed.pkg.version}`,
+        normalizedPref: wantedDependency.pref,
+        resolution: {
+          tarball: wantedDependency.pref,
+        },
+      }
+    }
+  }
+
+  return {
+    id: wantedDependency.pref
+      .replace(/^.*:\/\/(git@)?/, '')
+      .replace(/\.tgz$/, ''),
+      // TODO BREAKING CHANGE: uncomment the following: (or never remove extensions)
+      // .replace(/\.tar.gz$/, ''),
+      // .replace(/\.tar$/, ''),
+    normalizedPref: wantedDependency.pref,
+    resolution: {
+      tarball: wantedDependency.pref,
+    },
+  }
+}
