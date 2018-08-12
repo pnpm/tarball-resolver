8e056ae861dc5539a78eff4c61151e20b65a5b8e
Zoltan Kochan <z@kochan.io>
2018-08-12 15:13:43 +0300

    revert: tarballs from the default registry should be normalized
    This reverts commit fea450b45de58e464141d84c7205c60d468a2144

--- example.js
+++ example.js
@@ -1,9 +1,5 @@
 'use strict'
 const resolveFromTarball = require('@pnpm/tarball-resolver').default
 
-resolveFromTarball({
-  pref: 'https://registry.npmjs.org/is-array/-/is-array-1.0.1.tgz',
-}, {
-  registry: 'https://registry.npmjs.org',
-})
-.then(resolveResult => console.log(JSON.stringify(resolveResult, null, 2)))
+resolveFromTarball({pref: 'http://registry.npmjs.org/is-array/-/is-array-1.0.1.tgz'})
+  .then(resolveResult => console.log(JSON.stringify(resolveResult, null, 2)))
